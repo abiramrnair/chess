20b7e3b3108439ea5b81b5ca79c678f53ec8a078
Abiram Nair <abiramrnair@gmail.com>
2022-04-11 14:57:52 -0400

    Commit

--- frontend/client/index.js
+++
@@ -1,8 +0,0 @@
-// Babel has deprecated @babel/polyfill, and the following two imports are used for polyfills instead.
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import m from "mithril";
-import App from "./app";
-import "./styles/index.scss";
-
-m.mount(document.body, App);
